6cebc8ac7c8a14efaca676a63baea61492c88606
Gage Christensen <grchr4000@gmail.com>
2021-09-02 17:44:31 -0700

    Add opening filter
    The stats command for the CLI can now filter down games based on SAN
    moves passed at the end of the command.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use io::Read;
 use std::io;
 
-use clap::{App, SubCommand};
+use clap::{App, Arg, SubCommand};
 use pgn_reader::BufferedReader;
 
-use chesshound::{stats, AlgebraicMove, Game, GameParser, MoveTree};
+use chesshound::{stats, AlgebraicMove, Move, Game, GameParser, MoveTree};
 
 fn main() -> io::Result<()> {
     let matches = App::new("Chesshound")
@@ -12,29 +12,35 @@ fn main() -> io::Result<()> {
         .author("Gage C. <github.com/grchristensen>")
         .about("Chesshound is a CLI for finding patterns in sets of chess games")
         .subcommand(
-            SubCommand::with_name("stats").about("Gives statistics on game sets"),
-            // .arg(Arg::with_name("GAME_DIRECTORY")
-            //      .help("Sets the location to search for PGN files")
-            //      .required(true)
-            //      .index(1)
-            // )
+            SubCommand::with_name("stats").about("Gives statistics on game sets")
+            .arg(Arg::with_name("MOVES")
+                 .help("Filters games by moves played")
+                 .index(1)
+                 .multiple(true)
+            )
         )
         .get_matches();
 
-    if let Some(_matches) = matches.subcommand_matches("stats") {
+    if let Some(matches) = matches.subcommand_matches("stats") {
         let buffer = io::stdin()
             .lock()
             .bytes()
             .map(|b| b.unwrap())
             .collect::<Vec<_>>();
 
-        println!("{}", run_stats(&buffer)?);
+        let moves: Vec<String> = if let Some(values) = matches.values_of("MOVES") {
+            values.map(|move_| String::from(move_)).collect::<Vec<_>>()
+        } else {
+            Vec::new()
+        };
+
+        println!("{}", run_stats(&buffer, moves)?);
     }
 
     Ok(())
 }
 
-fn run_stats(pgn: &[u8]) -> io::Result<String> {
+fn run_stats(pgn: &[u8], moves: Vec<String>) -> io::Result<String> {
     let mut reader = BufferedReader::new_cursor(&pgn[..]);
 
     fn read_game<R: Read>(
@@ -52,10 +58,17 @@ fn run_stats(pgn: &[u8]) -> io::Result<String> {
         games.push(game);
     }
 
-    let game_count = games.len();
     let move_tree = MoveTree::new(games);
+    let mut move_tree_view = move_tree.view();
 
-    let (white_win_rate, black_win_rate, draw_rate) = stats::results(&mut move_tree.view().iter());
+    for move_ in moves {
+        move_tree_view = move_tree_view.with_next(&AlgebraicMove::from_algebraic(move_));
+    }
+
+    let filtered_games = move_tree_view.iter().collect::<Vec<_>>();
+    let game_count = filtered_games.len();
+
+    let (white_win_rate, black_win_rate, draw_rate) = stats::results(&mut filtered_games.into_iter());
     let white_win_percent = white_win_rate * 100.;
     let black_win_percent = black_win_rate * 100.;
     let draw_percent = draw_rate * 100.;
